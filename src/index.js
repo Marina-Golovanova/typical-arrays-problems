d7ee3f9ef2444fbaf049a6ff3f59f7371c47fc45
Marina-Golovanova <gma18@list.ru>
2021-03-10 10:35:19 +0300

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let minValue = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (minValue > array[i]) {
+        minValue = array[i]
+      }
+    }
+    return minValue;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let maxValue = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (maxValue < array[i]) {
+        maxValue = array[i]
+      }
+    }
+    return maxValue;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+    return 0;
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    return sum / array.length;
+  }
 }
